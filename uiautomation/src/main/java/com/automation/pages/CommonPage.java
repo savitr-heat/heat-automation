5ebf64c4eb592c6b5c8d6df4b70cbbafb4cde986
sravanthi-savitr <psravanthi@savitr.com>
2015-11-23 15:27:22 +0530

    update

---
+++ uiautomation/src/main/java/com/automation/pages/CommonPage.java
@@ -0,0 +1,55 @@
+package com.automation.pages;
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+
+import com.automation.framework.HeatWrapper;
+import com.automation.framework.Locators;
+
+public class CommonPage extends HeatWrapper{
+
+	public CommonPage(WebDriver driver) throws IOException{
+		super(driver);
+	}
+	public void LogoImage() throws Exception{
+		isElementDisplayed(Locators.Generic.Logo);
+	}
+	public void About() throws Exception{
+		
+		isElementDisplayed(Locators.Common.Help);
+		clickElement(Locators.Common.Help);
+		clickElement(Locators.Common.About);
+		isElementDisplayed(Locators.Common.AboutBox);
+	}
+	public void AboutTitle() throws Exception{
+		isElementDisplayed(Locators.Common.AboutBoxtitle);
+	}
+	public void AboutCloseIcon() throws Exception{
+		isElementDisplayed(Locators.Common.AboutBoxClose);
+	}
+	public void AboutBoxOk() throws Exception{
+		isElementDisplayed(Locators.Common.AboutBoxOk);
+	}
+	public void AboutBoxIcon() throws Exception{
+		isElementDisplayed(Locators.Common.AboutBoxIcon);
+	}
+	public void UserName() throws Exception{
+		isElementDisplayed(Locators.Common.UserName);
+	}
+	public void SubMenu() throws Exception{
+		clickElement(Locators.Common.UserName);
+		isElementDisplayed(Locators.Common.SubMenu);
+	}
+	public void SubMenuLinks()throws Exception{
+		isElementDisplayed(Locators.Common.IconPassword);
+		isElementDisplayed(Locators.Common.IconLocale);
+		isElementDisplayed(Locators.Common.IconPicture);
+	}
+	public void More() throws Exception {
+		isElementDisplayed(Locators.Common.More);
+		clickElement(Locators.Common.More);
+		isElementDisplayed(Locators.Common.MorePop);
+	}
+ 
+
+}
---
+++ uiautomation/src/test/java/com/automation/testcases/CommonTest.java
@@ -0,0 +1,76 @@
+package com.automation.testcases;
+
+import org.testng.annotations.Test;
+
+import com.automation.framework.BaseTest;
+
+
+
+public class CommonTest extends BaseTest{
+
+	/*public void LogoImageTest() throws Exception {
+		commonPage.loginToApp("connector.test", "conrotcd3$");
+		commonPage.LogoImage();
+
+	}
+	@Test
+	public void AboutTest() throws Exception{
+	commonPage.loginToApp("connector.test", "conrotcd3$");
+	commonPage.About();
+	}
+	@Test
+	public void AboutTitleTest() throws Exception{
+		commonPage.loginToApp("connector.test", "conrotcd3$");
+		commonPage.About();
+		commonPage.AboutTitle();		
+	}
+	@Test
+	public void AboutCloseTest() throws Exception{
+		commonPage.loginToApp("connector.test", "conrotcd3$");
+		commonPage.About();
+		commonPage.AboutCloseIcon();	
+	}
+	@Test
+	public void AboutOkTest() throws Exception{
+		commonPage.loginToApp("connector.test", "conrotcd3$");
+		commonPage.About();
+		commonPage.AboutBoxOk();		
+	}
+	@Test
+	public void AboutIconsTest() throws Exception{
+		commonPage.loginToApp("connector.test", "conrotcd3$");
+		commonPage.About();
+		commonPage.AboutBoxIcon();		
+	}
+	@Test
+	public void UserNameTest() throws Exception{
+		commonPage.loginToApp("connector.test", "conrotcd3$");
+		commonPage.UserName();
+	}
+	@Test
+	public void SubMenuTest() throws Exception{
+		commonPage.loginToApp("connector.test", "conrotcd3$");
+		commonPage.SubMenu();
+	}
+	@Test
+	public void SubIconsTest() throws Exception{
+		commonPage.loginToApp("connector.test", "conrotcd3$");
+		commonPage.UserName();
+		commonPage.SubMenu();
+		commonPage.SubMenuLinks();
+	}*/
+	@Test
+	public void MoreTest() throws Exception{
+		commonPage.loginToApp("connector.test", "conrotcd3$");
+		commonPage.More();
+		
+
+		
+	}
+	
+	
+
+
+
+
+}
